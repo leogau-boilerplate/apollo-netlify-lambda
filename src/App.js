d63875b79f36b23f5b676628f53f6efcb5c3fa73
Leo Gau <lgau@csod.com>
2018-09-14 12:57:24 -0700

    Add Apollo Server and Client

--- src/App.js
+++ src/App.js
@@ -2,30 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {loading: false, msg: null};
-  }
+import ApolloClient, {gql} from 'apollo-boost';
+import {ApolloProvider, Query} from 'react-apollo';
 
-  handleClick = (e) => {
-    e.preventDefault();
+const client = new ApolloClient({
+  uri: '/.netlify/functions/graphql'
+})
 
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
-  }
-
-  render() {
-    const {loading, msg} = this.state;
-
-    return <p>
-      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-      <span>{msg}</span>
-    </p>
-  }
-}
+const LambdaDemo = () => (
+  <ApolloProvider client={client}>
+    <Query
+      query={gql`
+        {
+          hello
+        }
+      `}
+    >
+      {({data}) => <div>A greeting from the server: {data.hello}</div>}
+    </Query>
+  </ApolloProvider>
+)
 
 class App extends Component {
   render() {
